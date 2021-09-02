eef13e4f21b5bf22a40f46319da79fd61529f8ff
Johnnatan Cortes Pizarro <jcortez@kdu.cl>
2021-09-02 03:30:07 -0400

    Initial

---
+++ SignalRDemo/Hubs/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.Others.SendAsync("RecieveMessage", message);
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/notificationhub").build();
+
+connection
+    .start()
+    .then(function () {
+        return console.log("Conectado!!!");
+    })
+    .catch(function (e) {
+        return console.error(e.toString());
+    });
+
+document.getElementById("eventBtn").onclick = sendEvent;
+
+function sendEvent() {
+    const message = document.getElementById("eventMessage").value;
+    connection
+        .invoke("SendMessage", message)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+};
+
+connection.on("RecieveMessage", function (message) {
+    console.log("Mensaje", message);
+});
